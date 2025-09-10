c0fd236743f5d6ad613936beb4bca9b46bffe982
deepdeb <debnath.pradeep619@gmail.com>
2025-09-10 23:33:56 +0530

    till download verification link

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,12 +4,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
+const db =
   globalForPrisma.prisma ??
   new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = db;
 }
 
-export default prisma;
+export default db;
